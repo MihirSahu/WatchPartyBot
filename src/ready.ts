b6b75b8b81ff9fdf32411e21e59a9a2f366a1c8c
Mihir Sahu <2002mihir@gmail.com>
2022-09-22 15:03:12 -0500

    Set up basic mongoose connection

--- src/ready.ts
+++ src/ready.ts
@@ -1,11 +1,14 @@
 import { Client } from "discord.js";
+import mongoose from 'mongoose';
 
 export default (client: Client): void => {
     client.on("ready", async () => {
         if (!client.user || !client.application) {
             return;
         }
-
         console.log(`${client.user.username} is online`);
+
+        await mongoose.connect('mongodb://localhost:27017/test');
+        console.log("Connected to database");
     });
 };
